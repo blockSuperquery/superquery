a9ee695bacd9f0475dea7786cadeefbc3ae3f597
0xsteins <olivedexofficial@gmail.com>
2026-07-06 11:50:24 +0100

    Refactor code structure for improved readability and maintainability

--- packages/node/src/subcommands/reindex.init.ts
+++
@@ -1,8 +0,0 @@
-// Copyright 2020-2025 SubQuery Pte Ltd authors & contributors
-// SPDX-License-Identifier: GPL-3.0
-
-import { reindexInit as baseReindexInit } from '@subql/node-core';
-import { ReindexModule } from './reindex.module';
-
-export const reindexInit = (targetHeight: number) =>
-  baseReindexInit(ReindexModule, targetHeight);
--- test/jest-setup.ts
+++
@@ -1,5 +0,0 @@
-import 'reflect-metadata';
-import 'regenerator-runtime/runtime';
-
-// Causes the logger to be init
-(global as any).__TEST__ = true;
